59015dd23cbba016ffbf6603079795369e85cf42
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-11 12:22:09 +0200

    Exercise 3.2 - Phonebook backend step2

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ const express = require('express')
 const app = express()
 
 const persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
 ]
 
+const requestTime = function (req, res, next) {
+  req.requestTime = new Date(Date.now()).toString()
+  next()
+}
+
+app.use(requestTime)
+
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${request.requestTime}</p>`
+  )
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
