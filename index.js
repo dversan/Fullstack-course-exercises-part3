5f4a69e6d0309c2351143fc985bd564a539a95fe
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-20 17:47:11 +0200

    WIP

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/info', (request, response, next) => {
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
