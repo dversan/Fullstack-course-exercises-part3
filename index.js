c10aff6e587d4980eed8daf2ec5c42e9fd882c40
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-20 14:48:41 +0200

    Exercise 3.16 - Phonebook database Step4

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ morgan.token('body', function (req) {
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'wrong id format' })
   }
@@ -19,18 +17,16 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' })
-// }
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(cors())
-app.use(express.static('dist'))
 app.use(express.json())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
-// app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
@@ -90,7 +86,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
-
   const person = { name: body.name, number: body.number }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
@@ -115,6 +110,9 @@ app.get('/info', (request, response, next) => {
     )
     .catch((error) => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
