c008554e424414c2fb1db7a7f7276c9619e569fc
DVerSan <davidverdeguersantolaria@gmail.com>
2023-10-25 17:09:50 +0200

    Exercise 4.4 - Helper functions and unit tests Step2

--- tests/blogsList.test.js
+++ tests/blogsList.test.js
@@ -1,8 +1,17 @@
 const listHelper = require('../utils/list_helper')
+const blogsSample = require('./dataSamples')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('Part4 Tests', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+
+  test('Total likes of all blogs should be 36', () => {
+    const result = listHelper.totalLikes(blogsSample)
+
+    expect(result).toBe(36)
+  })
 })
