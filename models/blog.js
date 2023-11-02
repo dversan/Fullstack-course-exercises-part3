202d285dbf6d896a869c48c0626a4cbd3e6e4a8e
DVerSan <davidverdeguersantolaria@gmail.com>
2023-11-02 09:45:20 +0100

    Exercise 4.14 - Blog list expansions Step2

--- models/blog.js
+++ models/blog.js
@@ -10,7 +10,11 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Blog URL is required']
   },
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
